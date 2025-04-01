9ab27c83d40b0271f9807357c27ce947fc97a427
Austin de Coup-Crank <austindcc@gmail.com>
2025-04-01 16:53:43 -0500

    add work on remote start/engine

--- src/vehiclepass/_types.py
+++ src/vehiclepass/_types.py
@@ -2,3 +2,4 @@ from typing import Literal
 
 HoodStatus = Literal["OPEN", "CLOSED"]
 CompassDirection = Literal["NORTH", "NORTHEAST", "EAST", "SOUTHEAST", "SOUTH", "SOUTHWEST", "WEST", "NORTHWEST"]
+VehicleCommand = Literal["remoteStart", "cancelRemoteStart", "lock", "unlock"]
