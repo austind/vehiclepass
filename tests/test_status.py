c4dcc8cb46323e888e4ea1989d609a7c084b1ee3
Austin de Coup-Crank <austindcc@gmail.com>
2025-04-02 18:58:49 -0500

    tweak tests

--- tests/test_status.py
+++ tests/test_status.py
@@ -10,35 +10,21 @@ from vehiclepass.units import Distance, Temperature
 from .conftest import mock_responses
 
 
-@mock_responses(status="status/remotely_started.json")
-def test_status_remotely_started(vehicle: Vehicle):
-    """Test status when vehicle is remotely started."""
-    assert vehicle.outside_temp is not None
-    assert isinstance(vehicle.outside_temp, Temperature)
-    assert isinstance(vehicle.odometer, Distance)
-    assert vehicle.outside_temp.c == 5.75
-    assert str(vehicle.outside_temp) == "42.35°F"
-    assert vehicle.is_running is True
-    assert vehicle.is_remotely_started is True
-    assert vehicle.shutoff_countdown.seconds == 1719.0
-    assert vehicle.shutoff_countdown.human_readable == "29m 39s"
-    assert vehicle.is_ignition_started is False
-    assert vehicle.fuel_level.percent == 0.74239368
-    assert str(vehicle.odometer) == "65580.12 mi"
-
-
-@mock_responses(status="status/off.json")
-def test_status_off(vehicle):
-    """Test status when vehicle is off."""
+@mock_responses(status="status/baseline.json")
+def test_properties(vehicle: Vehicle):
+    """Test baseline properties."""
     # Temperatures
     assert vehicle.outside_temp.c == 0.0
     assert vehicle.outside_temp.f == 32.0
+    assert str(vehicle.outside_temp) == "32.0°F"
     assert vehicle.engine_coolant_temp.c == 89.0
     assert vehicle.engine_coolant_temp.f == 192.2
+    assert str(vehicle.engine_coolant_temp) == "192.2°F"
 
     # Odometer
     assert vehicle.odometer.km == 105547.0
     assert vehicle.odometer.mi == 65583.84
+    assert str(vehicle.odometer) == "65583.84 mi"
 
     # Tire pressure
     assert vehicle.tire_pressure.front_left.psi == 39.45
@@ -55,8 +41,23 @@ def test_status_off(vehicle):
     assert vehicle.tire_pressure.rear_right.kpa == 275.0
 
     # Fuel
-    assert vehicle.fuel_level.percent == 0.72717624
-    assert str(vehicle.fuel_level) == "72.72%"
+    assert vehicle.fuel_level.percentage == 0.72717624
+    assert vehicle.fuel_level.percent == 0.73
+    assert str(vehicle.fuel_level) == "73.0%"
+
+    assert isinstance(vehicle.outside_temp, Temperature)
+    assert isinstance(vehicle.odometer, Distance)
+
+
+@mock_responses(status="status/remotely_started.json")
+def test_status_remotely_started(vehicle: Vehicle):
+    """Test status when vehicle is remotely started."""
+    assert vehicle.is_running is True
+    assert vehicle.is_remotely_started is True
+    assert vehicle.shutoff_countdown.seconds == 1719.0
+    assert vehicle.shutoff_countdown.human_readable == "29m 39s"
+    assert vehicle.is_ignition_started is False
+    assert vehicle.fuel_level.percent == 0.74
 
 
 @pytest.mark.parametrize("temp_c, temp_f", [(0, 32), (20, 68), (37, 98.6), (-10, 14)])
