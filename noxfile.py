729867a109711b057131d19e9f5bb184604760fe
Austin de Coup-Crank <austindcc@gmail.com>
2025-04-05 06:44:15 -0500

    Reduce duplication in noxfile

--- noxfile.py
+++ noxfile.py
@@ -4,14 +4,18 @@ import nox
 
 nox.options.default_venv_backend = "uv|virtualenv"
 
+UV_RUN = (
+    "uv",
+    "run",
+    "--active",  # Ensures that uv uses the virtualenv created by nox, not the project's venv
+)
+
 
 @nox.session(python="3.9")
 def type_checking(session):
     """Run type checking."""
     session.run(
-        "uv",
-        "run",
-        "--active",  # Ensures that uv uses the virtualenv created by nox, not the project's venv
+        *UV_RUN,
         "mypy",
         "src",
     )
@@ -21,9 +25,7 @@ def type_checking(session):
 def lint(session):
     """Run linting."""
     session.run(
-        "uv",
-        "run",
-        "--active",  # Ensures that uv uses the virtualenv created by nox, not the project's venv
+        *UV_RUN,
         "ruff",
         "check",
         ".",
@@ -34,8 +36,6 @@ def lint(session):
 def tests(session):
     """Run unit tests."""
     session.run(
-        "uv",
-        "run",
-        "--active",  # Ensures that uv uses the virtualenv created by nox, not the project's venv
+        *UV_RUN,
         "pytest",
     )
