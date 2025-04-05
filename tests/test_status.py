417a0189e9677059951713463d803ee4e8129c5f
Austin de Coup-Crank <austindcc@gmail.com>
2025-04-05 05:41:20 -0500

    Break up status tests

--- tests/test_status.py
+++ tests/test_status.py
@@ -3,30 +3,18 @@
 import pytest
 from respx import MockRouter
 
-from vehiclepass import Vehicle
+import vehiclepass
+from vehiclepass import units
 from vehiclepass.constants import AUTONOMIC_AUTH_URL, AUTONOMIC_TELEMETRY_BASE_URL
-from vehiclepass.units import Distance, Temperature
+from vehiclepass.tire_pressure import TirePressure
 
 from .conftest import mock_responses
 
 
 @mock_responses(status="status/baseline.json")
-def test_properties(vehicle: Vehicle):
-    """Test baseline properties."""
-    # Temperatures
-    assert vehicle.outside_temp.c == 0.0
-    assert vehicle.outside_temp.f == 32.0
-    assert str(vehicle.outside_temp) == "32.0°F"
-    assert vehicle.engine_coolant_temp.c == 89.0
-    assert vehicle.engine_coolant_temp.f == 192.2
-    assert str(vehicle.engine_coolant_temp) == "192.2°F"
-
-    # Odometer
-    assert vehicle.odometer.km == 105547.0
-    assert vehicle.odometer.mi == 65583.84
-    assert str(vehicle.odometer) == "65583.84 mi"
-
-    # Tire pressure
+def test_tire_pressure(vehicle: vehiclepass.Vehicle) -> None:
+    """Test tire pressure properties."""
+    assert isinstance(vehicle.tire_pressure, TirePressure)
     assert vehicle.tire_pressure.front_left.psi == 39.45
     assert str(vehicle.tire_pressure.front_left) == "39.45 psi"
     assert vehicle.tire_pressure.front_left.kpa == 272.0
@@ -40,28 +28,51 @@ def test_properties(vehicle: Vehicle):
     assert str(vehicle.tire_pressure.rear_right) == "39.89 psi"
     assert vehicle.tire_pressure.rear_right.kpa == 275.0
 
-    # Fuel
+
+@mock_responses(status="status/baseline.json")
+def test_temperature(vehicle: vehiclepass.Vehicle) -> None:
+    """Test temperature properties."""
+    assert isinstance(vehicle.outside_temp, units.Temperature)
+    assert vehicle.outside_temp.c == 0.0
+    assert vehicle.outside_temp.f == 32.0
+    assert str(vehicle.outside_temp) == "32.0°F"
+
+    assert isinstance(vehicle.engine_coolant_temp, units.Temperature)
+    assert vehicle.engine_coolant_temp.c == 89.0
+    assert vehicle.engine_coolant_temp.f == 192.2
+    assert str(vehicle.engine_coolant_temp) == "192.2°F"
+
+
+@mock_responses(status="status/baseline.json")
+def test_odometer(vehicle: vehiclepass.Vehicle) -> None:
+    """Test odometer properties."""
+    assert isinstance(vehicle.odometer, units.Distance)
+    assert vehicle.odometer.km == 105547.0
+    assert vehicle.odometer.mi == 65583.84
+    assert str(vehicle.odometer) == "65583.84 mi"
+
+
+@mock_responses(status="status/baseline.json")
+def test_fuel(vehicle: vehiclepass.Vehicle) -> None:
+    """Test fuel properties."""
+    assert isinstance(vehicle.fuel_level, units.Percentage)
     assert vehicle.fuel_level.percentage == 0.72717624
     assert vehicle.fuel_level.percent == 0.73
     assert str(vehicle.fuel_level) == "73.0%"
 
-    assert isinstance(vehicle.outside_temp, Temperature)
-    assert isinstance(vehicle.odometer, Distance)
-
 
 @mock_responses(status="status/remotely_started.json")
-def test_status_remotely_started(vehicle: Vehicle):
+def test_status_remotely_started(vehicle: vehiclepass.Vehicle) -> None:
     """Test status when vehicle is remotely started."""
     assert vehicle.is_running is True
     assert vehicle.is_remotely_started is True
     assert vehicle.shutoff_countdown.seconds == 851.0
     assert vehicle.shutoff_countdown.human_readable == "14m 11s"
     assert vehicle.is_ignition_started is False
-    assert vehicle.fuel_level.percent == 0.74
 
 
 @pytest.mark.parametrize("temp_c, temp_f", [(0, 32), (20, 68), (37, 98.6), (-10, 14)])
-def test_temperature_conversion(vehicle: Vehicle, mock_router: MockRouter, temp_c: float, temp_f: float):
+def test_temperature_conversion(vehicle: vehiclepass.Vehicle, mock_router: MockRouter, temp_c: float, temp_f: float):
     """Test that temperature conversions work correctly."""
     mock_router.post(AUTONOMIC_AUTH_URL).respond(
         json={"access_token": "mock-token-12345", "token_type": "Bearer", "expires_in": 3600}
